68a5d7b415ab5539aeb60a6f8120ff92f358d448
Daniel Hunter <Daniel.Hunter.HHC@Gmail.com>
2017-07-14 13:31:33 +0800

    Update DownloadfromContractID-Leaderboard.py

--- Scripts/DownloadfromContractID-Leaderboard.py
+++ Scripts/DownloadfromContractID-Leaderboard.py
@@ -10,7 +10,7 @@ with open("token.json", "r") as f:
     token = JD.decode(data)
 	
 #Need to figure out how to make the contract ID change-able?	
-path = " https://pc-service.hitman.io/profiles/page//leaderboardentries?contractid=%s&page=0 HTTP/1.1" % contractID
+path = " https://pc-service.hitman.io/profiles/page//leaderboardentries?contractid=%s&page=0" % contractID
 req = urllib.request(
     url=path
 )
