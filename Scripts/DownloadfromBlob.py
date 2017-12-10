d835e00d6b8d525ea367c1febe863198a2b392bd
Daniel Hunter <Daniel.Hunter.HHC@Gmail.com>
2017-12-10 10:58:55 +0800

    Update DownloadfromBlob.py

--- Scripts/DownloadfromBlob.py
+++ Scripts/DownloadfromBlob.py
@@ -21,7 +21,7 @@ req.add_header('Pragma','no-cache')
 req.add_header('Accept','application/json')
 req.add_header('Content-Type', 'application/json')
 req.add_header('User-Agent','G2 Http/1.0')
-req.add_header('Version','6.70.0')
+req.add_header('Version','6.71.0')
 
 r = urllib.request(req,
     timeout=5
